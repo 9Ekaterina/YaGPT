5c40422433a4e9f81baf7dfbfe11c6e759633e67
9Ekaterina <ekaterina.zhogoleva@gmail.com>
2024-03-25 20:36:32 +0300

    Добавили в промт дополнительные параметры

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 # Выполняем запрос к YandexGPT
-def ask_gpt(text):
+def ask_gpt(text, sys_text, geo_text):
     iam_token = 't1.9euelZqczZPHkYmclpqZisySyMuNj-3rnpWaisbGnJaRxo6dx5aTy5mazJ3l8_duOXlP-e9JDEY2_d3z9y5odk_570kMRjb9zef1656Vms2ekJCPjI6Wjo2Lz8aQm8-Y7_zF656Vms2ekJCPjI6Wjo2Lz8aQm8-YveuelZqczI-Vzo_MlZCdzpjHmJnGjLXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.noEkH-f_BZmlJ1jnUif4NM9lh_Giw1az75Hs1Dbmwmf8GenG-LK1U6Op7c6fO0VeL6hfPY9NePSk9agONVDODg'  # Токен для доступа к YandexGPT
     folder_id = 'b1gfe8mbuq037jbemc92'  # Folder_id для доступа к YandexGPT
 
@@ -18,6 +18,16 @@ def ask_gpt(text):
             "maxTokens": "200"  # максимальное число сгенерированных токенов, очень важный параметр для экономии токенов
         },
         "messages": [
+            {
+                "role": "system",
+                "text": sys_text
+            }
+            ,
+            {
+                "role": "assistant",
+                "text": geo_text
+            }
+            ,
             {
                 "role": "user",  # пользователь спрашивает у модели
                 "text": text # передаём текст, на который модель будет отвечать
@@ -46,6 +56,8 @@ def ask_gpt(text):
 
 
 
-prompt_gpt = input("Про что будем писать рассказ?")
+prompt_gpt = input("Про что будем писать рассказ? ")
+theme_gpt = input("В какой тематике должен быть рассказ? ")
+location_gpt = input("Где будет происходить действие? ")
 
-ask_gpt(prompt_gpt)
+ask_gpt(prompt_gpt,theme_gpt, location_gpt)
