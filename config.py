8a061777f8c7b84eac962ea76447dac6030caeb8
9Ekaterina <ekaterina.zhogoleva@gmail.com>
2024-03-28 08:35:41 +0300

    Базовый конфигурационный файл для выгрузки в github

---
+++ config.py
@@ -0,0 +1,17 @@
+#Telegram bot
+TOKEN = 'YOU_TOKEN'
+
+#Константы для бота
+MAX_PROJECT_TOKENS = 15000 # макс. количество токенов на весь проект
+MAX_USERS = 2 # макс. количество пользователей на весь проект
+MAX_SESSIONS = 3 # макс. количество сессий у пользователя
+MAX_TOKENS_IN_SESSION = 1000 # макс. количество токенов за сессию пользователя
+MAX_MODEL_TOKENS = 200 # максимальное количество токенов в ответе нейросети
+
+
+#YandexGPT
+YATOKEN = 'YOU_TOKEN_YAGPT'
+FID = 'YOU_YAGPT_FOLDER'
+
+#SQL
+DATABASE = 'sqlite_table.db'
